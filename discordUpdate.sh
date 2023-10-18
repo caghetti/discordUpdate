cda6ebec01dd7e14f20b80d01bf02746f492e314
Charly AGHETTI <charly@ganti.io>
2023-10-18 21:23:55 +0200

    retrieve new discord version via url instead to check it via wget

--- discordUpdate.sh
+++ discordUpdate.sh
@@ -5,32 +5,12 @@ baseUri=https://dl.discordapp.net/apps/linux
 fullpath=`find /home/caghetti/.config/discord -type d -name '*.*.*'`
 currentVersion=`basename $fullpath`
 
-IFS='.' read -r -a components <<< "$currentVersion"
-majorVersion=${components[0]}
-minorVersion=${components[1]}
-patchVersion=${components[2]}
-
 
 cd /tmp/
-newVersion=$majorVersion.$minorVersion.$((${patchVersion} + 1))
-wget $baseUri/$newVersion/discord-$newVersion.deb 1>/dev/null 2>/dev/null
-if [ -f /tmp/discord-$newVersion.deb ]
-then
-  sudo dpkg -i /tmp/discord-$newVersion.deb
-fi
-
-newVersion=$majorVersion.$((${minorVersion} +1)).0
-wget $baseUri/$newVersion/discord-$newVersion.deb 1>/dev/null 2>/dev/null
-if [ -f /tmp/discord-$newVersion.deb ]
-then
-  sudo dpkg -i /tmp/discord-$newVersion.deb
-fi
-
-newVersion=$((${majorVersion} +1)).0.0
-wget $baseUri/$newVersion/discord-$newVersion.deb 1>/dev/null 2>/dev/null
-if [ -f /tmp/discord-$newVersion.deb ]
-then
-  sudo dpkg -i /tmp/discord-$newVersion.deb
+newVersion=`url -I -s 'https://discord.com/api/download?platform=linux&format=deb' | grep -i '^Location:' | sed -n 's/.*\/\([0-9.]*\)\/.*/\1/p'`
+if [ "$currentVersion" != "$newVersion" ]; then
+    wget $baseUri/$newVersion/discord-$newVersion.deb 1>/dev/null 2>/dev/null
+    sudo dpkg -i /tmp/discord-$newVersion.deb
 fi
 
 /usr/bin/discord
