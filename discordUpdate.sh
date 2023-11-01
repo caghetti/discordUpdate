379ea47b2be0009dd77e9a77e8986e751fec770f
Charly AGHETTI <charly@ganti.io>
2023-11-01 11:40:58 +0100

    error in curl call

--- discordUpdate.sh
+++ discordUpdate.sh
@@ -7,7 +7,7 @@ currentVersion=`basename $fullpath`
 
 
 cd /tmp/
-newVersion=`url -I -s 'https://discord.com/api/download?platform=linux&format=deb' | grep -i '^Location:' | sed -n 's/.*\/\([0-9.]*\)\/.*/\1/p'`
+newVersion=`curl -I -s 'https://discord.com/api/download?platform=linux&format=deb' | grep -i '^Location:' | sed -n 's/.*\/\([0-9.]*\)\/.*/\1/p'`
 if [ "$currentVersion" != "$newVersion" ]; then
     wget $baseUri/$newVersion/discord-$newVersion.deb 1>/dev/null 2>/dev/null
     sudo dpkg -i /tmp/discord-$newVersion.deb
